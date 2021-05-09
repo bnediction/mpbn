ddee376600e17cee00cea8bd6b89b374e22b2196
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-05-09 16:28:38 +0200

    clingo is on pypi

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name=NAME,
     long_description_content_type="text/markdown",
     install_requires=[
         "boolean.py",
+        "clingo",
         "colomoto_jupyter>=0.8.0",
     ],
     entry_points={
