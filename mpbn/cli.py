c57a5e34425db8864277fbe06a414473cf243c07
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2021-09-07 22:59:46 +0200

    cli: add fixedpoints sub-command

--- mpbn/cli.py
+++ mpbn/cli.py
@@ -7,11 +7,14 @@ from argparse import ArgumentParser
 def main():
     ap = ArgumentParser(prog=sys.argv[0])
     ap.add_argument("bnet_file")
-    ap.add_argument("method", choices=["attractors", "bn2asp"])
+    ap.add_argument("method", choices=["attractors", "fixedpoints", "bn2asp"])
     args = ap.parse_args()
     mbn = mpbn.MPBooleanNetwork(args.bnet_file)
     if args.method == "attractors":
-        for attractor in mbn.attractors(yield_=True):
+        for attractor in mbn.attractors():
+            print(attractor)
+    elif args.method == "fixedpointss":
+        for attractor in mbn.fixedpoints():
             print(attractor)
     elif args.method == "bn2asp":
         print(mbn.asp_of_bn())
