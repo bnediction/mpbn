f36c95cae8bb1094b683b958669384c9ec2910bd
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2022-11-18 16:15:39 +0100

    mpbn cli: add --limit option

--- mpbn/cli/__init__.py
+++ mpbn/cli/__init__.py
@@ -8,13 +8,15 @@ def main():
     ap = ArgumentParser(prog=sys.argv[0])
     ap.add_argument("bnet_file")
     ap.add_argument("method", choices=["attractors", "fixedpoints", "bn2asp"])
+    ap.add_argument("--limit", type=int, default=0,
+                    help="limit the number of results")
     args = ap.parse_args()
     mbn = mpbn.MPBooleanNetwork(args.bnet_file)
     if args.method == "attractors":
-        for attractor in mbn.attractors():
+        for attractor in mbn.attractors(limit=args.limit):
             print(attractor)
     elif args.method == "fixedpoints":
-        for attractor in mbn.fixedpoints():
+        for attractor in mbn.fixedpoints(limit=args.limit):
             print(attractor)
     elif args.method == "bn2asp":
         print(mbn.asp_of_bn())
